15a986a889694d397101497be8581e0dc213bb63
ZeroBeyond <zerobeyond@yeah.net>
2022-09-14 05:29:10 +0800

    Start the basic daemon

--- command_daemon.py
+++ command_daemon.py
@@ -12,12 +12,12 @@ from flask import Flask
 
 # constants 
 work_dir='/var/tmp/cmd_daemon' 
+if not os.path.exists(work_dir):
+    os.mkdir(work_dir)
 
-
-def program_init():
-    if not os.path.exists(work_dir):
-        os.mkdir(work_dir)
-
+stderr_file = open('/var/tmp/cmd_daemon/stderr.txt', 'w+')
+stdout_file = open('/var/tmp/cmd_daemon/stdout.txt', 'w+')
+log_file = open('/var/tmp/cmd_daemon/daemon-log.txt', 'w+')
 
 def program_cleanup():
     pass
@@ -25,10 +25,13 @@ def program_cleanup():
 def reload_program_config():
     pass
 
+
 context = daemon.DaemonContext(
     working_directory=work_dir,
     umask=0o002,
-    pidfile=lockfile.FileLock('/var/run/cmd_daemon.pid'),
+    pidfile=lockfile.FileLock('/var/tmp/cmd_daemon/lock.pid'),
+    stderr=stderr_file,
+    stdout=stdout_file
     )
 
 context.signal_map = {
@@ -38,23 +41,45 @@ context.signal_map = {
     }
 
 cmd_file = open('cmd.sh', 'w')
-context.files_preserve = [cmd_file]
+
+context.files_preserve = [cmd_file, log_file]
 
 def exec():
     subprocess.call("cmd.sh")
 
-# start the main program
-app=Flask(__name__) 
-@app.route('/', methods=["POST"]) 
-def func():  
-    if request.method == 'POST':
-        cmd = request.form['command']
-        if cmd == 'exec':
-            exec()
-    else:
+def create_app():
+    # start the main program
+    app=Flask(__name__) 
+    @app.route('/', methods=["POST", "GET"]) 
+    def func():  
+        log_file.write("aaaaaaaa\n")
+        if request.method == 'POST':
+            cmd = request.form['command']
+            if cmd == 'exec':
+                exec()
+        else:
+            return "Hello CmdDaemon!"
+
+    return app
+
+
+def create_app1():
+    # start the main program
+    app=Flask(__name__) 
+    @app.route('/') 
+    def func():  
         return "Hello CmdDaemon!"
 
-program_init()
+    return app
+
+def logging_forever(log_file):
+    while True:
+        print("Daemon")
+        log_file.write("Logging...")
+
+def run_web_app():
+    app = create_app1()
+    app.run(host = '0.0.0.0', port=17777)
 
 with context:
-    app.run(host = '0.0.0.0', port=17777)
+    logging_forever(log_file)
